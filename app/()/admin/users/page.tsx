52d0c0bb54a28ce24fe87f122b9812a00ca8830b
canhta <canh.tc98@gmail.com>
2023-10-08 00:19:49 +0700

    Add user/stream deletion features

--- app/()/admin/users/page.tsx
+++ app/()/admin/users/page.tsx
@@ -1,14 +1,23 @@
 'use client';
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
+import Link from 'next/link';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/navigation';
 import Loading from '@/components/common/Loading';
 import Breadcrumb from '@/components/common/Breadcrumb';
-import Link from 'next/link';
 import { IRoute } from '@/utils/interfaces/system';
 import { IUser } from '@/utils/interfaces/user';
 import Table from '@/components/common/Table';
+import EditIcon from '@/components/icons/EditIcon';
+import DeleteIcon from '@/components/icons/DeleteIcon';
 import { ColumnDef } from '@tanstack/react-table';
+import ConfirmModal from '@/components/common/ConfirmModal';
+
+const htmlForDelete = 'delete-user-modal';
 
 export default function Page() {
+  const router = useRouter();
+  const [deletingID, setDeletingID] = useState<IUser['id'] | null>(null);
   const routes: IRoute[] = [
     { title: 'Home', url: '/' },
     { title: 'User Management', url: '' },
@@ -26,6 +35,22 @@ export default function Page() {
 
     return { data, total };
   }
+
+  const onDeleteSubmit = async () => {
+    if (deletingID) {
+      const res = await fetch(`/api/v1/users/${deletingID}`, {
+        method: 'DELETE',
+      });
+
+      if (!res.ok) {
+        toast.error('Something went wrong, please try again later.');
+      } else {
+        toast.success('User deleted successfully.');
+
+        // TODO: Reload data
+      }
+    }
+  };
 
   const columns = React.useMemo<ColumnDef<IUser>[]>(
     () => [
@@ -46,8 +71,35 @@ export default function Page() {
           );
         },
       },
+      {
+        header: 'Actions',
+        cell(props) {
+          const onEditClick = () => {
+            router.push(`/admin/users/${props.row.original.id}`);
+          };
+
+          const onDeleteClicked = () => {
+            setDeletingID(props.row.original.id);
+          };
+
+          return (
+            <div className="flex gap-2">
+              <button className="btn btn-sm btn-outline" onClick={onEditClick}>
+                <EditIcon />
+              </button>
+              <label
+                className="btn btn-sm btn-outline btn-error"
+                htmlFor={htmlForDelete}
+                onClick={onDeleteClicked}
+              >
+                <DeleteIcon />
+              </label>
+            </div>
+          );
+        },
+      },
     ],
-    [],
+    [router],
   );
 
   return (
@@ -67,6 +119,15 @@ export default function Page() {
           <Table columns={columns} fetchData={fetchData} />
         </div>
       }
+      <ConfirmModal
+        htmlFor={htmlForDelete}
+        title={'Delete User'}
+        content={'Are you sure you want to delete this user?'}
+        onClose={() => {
+          setDeletingID(null);
+        }}
+        onSubmit={onDeleteSubmit}
+      />
     </Suspense>
   );
 }
