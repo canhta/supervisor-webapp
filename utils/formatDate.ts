de1335fa475cc24dfd8e240add087bf794b6e135
canhta <canh.tc98@gmail.com>
2023-10-07 15:41:20 +0700

    Fix timezone issue

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon';
 
 export function formatDate(date: string) {
-  return DateTime.fromISO(date).toLocaleString(
-    DateTime.DATETIME_SHORT_WITH_SECONDS,
-  );
+  return DateTime.fromISO(date)
+    .plus({ hours: 7 }) // TODO: For now Database is deploying on Singapore with timezone +7
+    .toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS);
 }
