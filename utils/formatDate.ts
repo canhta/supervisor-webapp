337b7c9719c62efd3926e1e982b9d73371c65164
canhta <canh.tc98@gmail.com>
2023-10-08 13:01:06 +0700

    Update date format

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -1,8 +1,9 @@
 import { DateTime } from 'luxon';
 
 function formatDate(date: string) {
-  const newDate = DateTime.fromISO(date);
-  return newDate.toFormat('dd - LL - yyyy');
+  return DateTime.fromISO(date)
+    .plus({ hours: 7 }) // TODO: Currently the timezone is based on Database timezone (For now is Singapore UTC+7)
+    .toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS);
 }
 
 export { formatDate };
